c663549641c025897fa6420da9003cf7fd0cf0c8
debasishtech <debasish.palkitravels@gmail.com>
2026-02-26 13:35:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+/* ===== HAMBURGER MENU ===== */
+
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', ()=>{
+    nav.classList.toggle('active');
+    toggle.classList.toggle('active');
+});
+
+/* ===== SCROLL ANIMATION ===== */
+
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll("section").forEach(sec=>{
+    observer.observe(sec);
+});
+
+/* ===== MOBILE ACTIVE TAB ON SCROLL ===== */
+
+if (window.innerWidth <= 768) {
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+function setActiveLink() {
+
+let scrollPos = window.scrollY + 140;
+
+sections.forEach(section => {
+
+const top = section.offsetTop;
+const bottom = top + section.offsetHeight;
+const id = section.getAttribute("id");
+
+if (scrollPos >= top && scrollPos < bottom) {
+
+navLinks.forEach(link => link.classList.remove("active-link"));
+
+const activeLink = document.querySelector(
+'.nav-links a[href="#' + id + '"]'
+);
+
+if (activeLink) activeLink.classList.add("active-link");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll", setActiveLink);
+setActiveLink();
+
+}
